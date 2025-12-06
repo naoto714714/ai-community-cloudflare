f6be8254039bb86a322314d7702b653ad9415d08
naoto714714 <kimura.naoto5@gmail.com>
2025-12-06 21:39:15 +0900

    feature: functionsを使ってみる

---
+++ functions/helloworld.ts
@@ -0,0 +1,11 @@
+export function onRequest() {
+    return new Response(
+      JSON.stringify({ message: "Hello, World!" }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
