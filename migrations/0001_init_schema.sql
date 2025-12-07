25b218ff24d585a090ea1c4fac83b128435b9654
naoto714714 <kimura.naoto5@gmail.com>
2025-12-07 18:54:48 +0900

    chore: マイグレーションを1本化

--- migrations/0001_init_schema.sql
+++ migrations/0001_init_schema.sql
@@ -1,4 +1,17 @@
--- Migration number: 0001 	 2025-12-06T13:02:05.245Z
+-- Migration number: 0001   2025-12-07T00:00:00.000Z
+
+CREATE TABLE channels (
+  id          INTEGER PRIMARY KEY AUTOINCREMENT,
+  created_at  INTEGER NOT NULL DEFAULT (unixepoch()),
+  channel_id  TEXT NOT NULL UNIQUE,
+  description TEXT DEFAULT '',
+  members     TEXT NOT NULL DEFAULT '[]'
+);
+
+INSERT INTO channels (channel_id, description, members)
+VALUES
+  ('雑談', '何でも自由に話せる場所', json('["u1", "u2", "u3", "u4", "gemini", "me"]')),
+  ('ニュース', '最新のニュースや情報', json('["u1", "u5", "gemini", "me"]'));
 
 CREATE TABLE messages (
   id          INTEGER PRIMARY KEY AUTOINCREMENT,
